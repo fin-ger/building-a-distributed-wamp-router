df4019ac076029b8a4d54cbbef7a7690597b8dc6
Fin Christensen <christensen.fin@gmail.com>
2019-06-14 18:11:13 +0200

    Add crossbar deployment

---
+++ scenarios/availability/run.sh
@@ -0,0 +1,151 @@
+#! /bin/sh
+
+set -e
+
+fail() {
+    printf "\e[1;31m !!! \e[1;0m%s\e[0m" "$@"
+    exit 1
+}
+
+PROJECT_ROOT="../.."
+SCENARIO_NAME="availability"
+SCENARIO="scenario-${SCENARIO_NAME}"
+
+autobahnkreuz_up() {
+    # create subshell to prevent changing PWD
+    (
+        mkdir -p "${PROJECT_ROOT}/vendor"
+        cd "${PROJECT_ROOT}/vendor"
+
+        rm -rf autobahnkreuz
+        svn export "https://github.com/verkehrsministerium/autobahnkreuz-rs.git/trunk/dist/helm"
+        mv helm autobahnkreuz
+
+        cd autobahnkreuz
+
+        # it does not matter when this fails
+        helm delete --purge autobahnkreuz || true
+        helm install --name autobahnkreuz . || fail "failed to install autobahnkreuz chart"
+    )
+}
+
+autobahnkreuz_down() {
+    helm delete --purge autobahnkreuz || fail "failed to delete autobahnkreuz chart"
+}
+
+crossbar_up() {
+    # create subshell to prevent changing PWD
+    (
+        cd "${PROJECT_ROOT}/deployments/crossbar" || fail "crossbar helm chart not found"
+
+        # it does not matter when this fails
+        helm delete --purge crossbar || true
+        helm install --name crossbar . || fail "failed to install crossbar chart"
+    )
+}
+
+crossbar_down() {
+    helm delete --purge crossbar || fail "failed to delete crossbar chart"
+}
+
+emitter_up() {
+    fail "not implemented yet"
+}
+
+emitter_down() {
+    fail "not implemented yet"
+}
+
+wamp_up() {
+    WAMP_ADDRESS="$1"
+
+    # create subshell to prevent changing PWD
+    (
+        cd "${PROJECT_ROOT}/scenarios/${SCENARIO_NAME}/wamp" || fail "${SCENARIO} helm chart not found"
+
+        # it does not matter when this fails
+        helm delete --purge "${SCENARIO}" || true
+        helm install --set "routerAddress=${WAMP_ADDRESS}" --name "${SCENARIO}" . \
+            || fail "failed to install ${SCENARIO} chart"
+    )
+}
+
+wamp_down() {
+    helm delete --purge "${SCENARIO}" || fail "failed to delete ${SCENARIO} chart"
+}
+
+mqtt_up() {
+    fail "not implemented yet"
+}
+
+mqtt_down() {
+    fail "not implemented yet"
+}
+
+kill_random_pod() {
+    SERVICE_NAME="$1"
+
+    POD="$(kubectl get pods --selector "app.kubernetes.io/name=${SERVICE_NAME}" \
+                   --output custom-columns=:.metadata.name \
+                   --no-headers | shuf -n 1)"
+
+    kubectl delete pod --force --grace-period=0 "${POD}"
+}
+
+run() {
+    ROUTER="$1"
+    LENGTH="$(date -d "now +5 min" +%s)"
+
+    case "${ROUTER}" in
+        autobahnkreuz)
+            autobahnkreuz_up
+            wamp_up "ws://autobahnkreuz:80"
+
+            while [ "${LENGTH}" -ge "$(date +%s)" ]
+            do
+                kill_random_pod "autobahnkreuz"
+                sleep 1
+            done
+
+            wamp_down
+            autobahnkreuz_down
+
+            return
+            ;;
+        crossbar)
+            crossbar_up
+            sleep 10
+            wamp_up "ws://crossbar:80/ws"
+
+            while [ "${LENGTH}" -ge "$(date +%s)" ]
+            do
+                kill_random_pod "crossbar"
+                sleep 1
+            done
+
+            #wamp_down
+            #crossbar_down
+
+            return
+            ;;
+        emitter)
+            emitter_up
+            mqtt_up
+
+            while [ "${LENGTH}" -ge "$(date +%s)" ]
+            do
+                  kill_random_pod "emitter"
+                  sleep 1
+            done
+
+            mqtt_down
+            emitter_down
+
+            return
+            ;;
+        *)
+            fail "invalid router ${ROUTER}"
+    esac
+}
+
+run crossbar
--- scenarios/availability/wamp/index.js
+++ scenarios/availability/wamp/index.js
@@ -17,25 +17,24 @@ function snooze(ms) {
 }
 
 async function main() {
-    const connection = new Connection({
-        endpoint: ROUTER_ADDRESS,
-        realm: 'default',
-
-        serializer: new JSONSerializer(),
-        transport: NodeWebSocketTransport,
-        authProvider: new AnonymousAuthProvider(),
-
-        logFunction: (level, timestamp, file, msg) => {
-            if (level === 'INFO' || level === 'WARNING' || level === 'ERROR') {
-                console.log(level, timestamp, file, msg);
-            }
-        },
-    });
-
     const hostname = os.hostname();
     while (true) {
         const timestamp = getTimestamp();
         try {
+            const connection = new Connection({
+                endpoint: ROUTER_ADDRESS,
+                realm: 'default',
+
+                serializer: new JSONSerializer(),
+                transport: NodeWebSocketTransport,
+                authProvider: new AnonymousAuthProvider(),
+
+                logFunction: (level, timestamp, file, msg) => {
+                    if (level === 'INFO' || level === 'WARNING' || level === 'ERROR') {
+                        console.log(level, timestamp, file, msg);
+                    }
+                },
+            });
             await connection.Open();
             await connection.Close();
             console.log(`${hostname},${timestamp},1`);
