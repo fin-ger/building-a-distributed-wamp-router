5ed6b6b4f1a555138a88d88537bfd96c74e3f414
Fin Christensen <christensen.fin@gmail.com>
2019-06-17 16:00:06 +0200

    Add scaling-out plot

--- scenarios/scaling-out/plot.py
+++ scenarios/scaling-out/plot.py
@@ -8,17 +8,17 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 
 path = sys.argv[1]
-plt.title('latency when publishing topics with 10 clients')
+plt.title('messages per second when increasing replica count every minute')
 plt.xlabel('time in seconds')
-plt.ylabel('latency in µs')
+plt.ylabel('messages per second')
 
 for filename in glob.glob(path + "-*.csv"):
     router = re.match(".*-(\w+).csv", filename).group(1)
 
-    data = np.genfromtxt(filename, delimiter=',', names=['time', 'name', 'latency'], dtype=None)
+    data = np.genfromtxt(filename, delimiter=',', names=['name', 'time', 'msgs'], dtype=None)
     t0 = min(data['time'])
     times = np.array([(t - t0) / 1000 for t in data['time']])
-    plt.plot(times, data['latency'], label=router)
+    plt.plot(times, data['msgs'], label=router, marker='.', linestyle='')
 
 plt.legend(loc='best')
 plt.savefig(path + ".png")
