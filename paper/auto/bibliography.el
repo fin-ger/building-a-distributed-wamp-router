b4c5c4a1fcf34a970b24efa440b2629caf696a62
Fin Christensen <christensen.fin@gmail.com>
2019-06-28 19:10:58 +0200

    Add conclusion and future work

---
+++ paper/auto/bibliography.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "bibliography"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "WAMP:2019"
+    "CFX:2019"
+    "Ongaro:2014"
+    "WAMP_session:2019"
+    "DOCKER:2019"
+    "ALPINE:2018"
+    "RFC6202"))
+ :bibtex)
+
